0de931c4d1a72ed8b73bf8244f312bf3ac80ce3b
Kris Zyp <kriszyp@gmail.com>
2010-07-06 09:27:33 -0600

    Removed files that are now in promised-io

--- engines/rhino/lib/fs-promise.js
+++
@@ -1,7 +0,0 @@
-var File = require("file");
-exports.readFileSync = exports.read = File.read;
-exports.writeFileSync = exports.write = File.write;
-exports.statSync = File.stat;
-
-exports.makeTree = File.mkdirs;
-exports.makeDirectory = File.mkdir;
